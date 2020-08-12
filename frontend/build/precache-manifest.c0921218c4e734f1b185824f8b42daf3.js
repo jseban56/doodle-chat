f8b59139ee8bdf587c103a325483905a8ea7e6a2
Joel Andersson <joel@acc.umu.se>
2020-08-12 09:18:51 +0200

    Initial commit, project setup in place.
    Separate folders/Docker files for frontend and backend, with one docker-compose
    in root for running both.
    
    Can be run by simply: `docker-compose up`

---
+++ frontend/build/precache-manifest.c0921218c4e734f1b185824f8b42daf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "55fce347f9d5c0291be5",
+    "url": "/static/css/main.e9d8b6ad.chunk.css"
+  },
+  {
+    "revision": "55fce347f9d5c0291be5",
+    "url": "/static/js/main.65623ffb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "52ea22591633bb07d69baa6b50d26eab",
+    "url": "/index.html"
+  }
+];
